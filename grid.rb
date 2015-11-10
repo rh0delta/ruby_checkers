9a0d843984996b08485d7a49251ffad9110848c1
Nayyir Jutha <nayyir.jutha@gmail.com>
2015-11-10 15:07:29 +0000

    Seperated the creation of game board from initializer

--- grid.rb
+++ grid.rb
@@ -4,6 +4,9 @@ class Grid
     @board = board
     @end_row = end_row
     @end_column = end_column
+  end
+
+  def create_board
     letters = ('a'..@end_row).to_a
     numbers = (1..@end_column).to_a
     # puts letters.inspect
@@ -12,25 +15,25 @@ class Grid
     even = middle[0]
     odd = middle[1]
     letters.each do |letter|
-        numbers.each do |i|
-          if i % 2 == 0 && letter.match(/a/)
-            @board["#{letter}#{i}"] = "b |"
-          elsif i % 2 != 0 && letter.match(/b/)
-            @board["#{letter}#{i}"] = "b |"
-          elsif i % 2 == 0 && letter.match(letters.last(2)[0])
-            @board["#{letter}#{i}"] = "w |"
-          elsif i % 2 != 0 && letter.match(letters.last(2)[1])
-            @board["#{letter}#{i}"] = "w |"
-          elsif i % 2 != 0 && odd.include?(letter)
-            @board["#{letter}#{i}"] = "  |"
-          elsif i % 2 == 0 && even.include?(letter)
-            @board["#{letter}#{i}"] = "  |"
-          else
-            @board["#{letter}#{i}"] = "  |"
-          end
-          print @board["#{letter}#{i}"]
+      numbers.each do |i|
+        if i % 2 == 0 && letter.match(/a/)
+          @board["#{letter}#{i}"] = "b |"
+        elsif i % 2 != 0 && letter.match(/b/)
+          @board["#{letter}#{i}"] = "b |"
+        elsif i % 2 == 0 && letter.match(letters.last(2)[0])
+          @board["#{letter}#{i}"] = "w |"
+        elsif i % 2 != 0 && letter.match(letters.last(2)[1])
+          @board["#{letter}#{i}"] = "w |"
+        elsif i % 2 != 0 && odd.include?(letter)
+          @board["#{letter}#{i}"] = "  |"
+        elsif i % 2 == 0 && even.include?(letter)
+          @board["#{letter}#{i}"] = "  |"
+        else
+          @board["#{letter}#{i}"] = "  |"
         end
-        puts
+        print @board["#{letter}#{i}"]
       end
+      puts
     end
   end
+end
--- main.rb
+++ main.rb
@@ -2,10 +2,9 @@ require_relative 'game.rb'
 require_relative 'grid.rb'
 require_relative 'player.rb'
 
-@grid = {}
-
+@grid={}
 game_board = Grid.new(@grid, 'g', 8)
 player1 = Player.new('Nayyir')
 player2 = Player.new('Frank')
 
-game = Game.new(players: [player1, player2], board: game_board)
+# game = Game.new(players: [player1, player2], board: game_board.create_board)
